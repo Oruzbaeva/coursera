8a927058e3db40d6f8f3120cb017802bcfe44eae
Salta <salta_95@inbox.ru>
2023-05-27 17:19:46 +0200

    Added model

---
+++ app/models.py
@@ -0,0 +1,11 @@
+
+class User:
+
+    def __init__(self, first_name, last_name) -> None:
+        self.first_name = first_name
+        self.last_name = last_name
+
+    def hi(self):
+        print(f'Hi my name is {self.last_name + self.first_name} !')
+
+        
